981170a4e5fdb87290b7c17f9d22bddbccae8ab7
Paweł Socała <psocala12@gmail.com>
2024-10-08 19:42:06 +0200

    grid v2

--- Grid/grid.cpp
+++ Grid/grid.cpp
@@ -1,31 +1,114 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using std::cout;
 using std::endl;
 using std::ifstream;
+using std::vector;
 
-
-class Node {
+class Node { // węzły siatki oraz ich współrzędne
     private:
         int x, y;
+    public:
+        Node() {}
+        Node(int p_x, int p_y) : x(p_x), y(p_y) {}
+
+        int get_x() { return x; }
+        int get_y() { return y; }
+
+        void display_node() {
+            cout << "(" << x << ", " << y  << ")" << endl;
+        }
 };
 
-class Element {
+class Element { // elementy tworzone przez 4 węzły (obiekty Node)
     private:
         int ID[4];
+    public:
+        Element() {
+            for (int i = 0; i < 4; i++) {
+                ID[i] = 0;
+            }
+        }
+
+        void set_ID(int node_1, int node_2, int node_3, int node_4) {
+            ID[0] = node_1;
+            ID[1] = node_2;
+            ID[2] = node_3;
+            ID[3] = node_4;
+        }
+
+        void display_ID() {
+            for (int i = 0; i < sizeof(ID) / sizeof(ID[0]); i++) {
+                cout << ID[i] << " ";
+            }
+            cout << endl;
+        }
 };
 
-class Grid {
+class Grid { // siatka tworzona z elementów i węzłów
     private:
-        int nN, nE;
-        // element[nE]
-        // node [nN]
+        int nN, nE, nW, nH, height, width;
+        vector<Node> nodes;
+        vector<Element> elements;
+
+    public:
+        Grid() {}
+        Grid(int p_nN, int p_nE, int p_nW, int p_nH, int p_height, int p_width) : nN(p_nN), nE(p_nE), nW(p_nW), nH(p_nH), height(p_height), width(p_width) {
+            create_nodes();
+            create_elements();
+        }
+
+        void create_nodes() {
+            double delta_x = width / (nW - 1);  
+            double delta_y = height / (nH - 1); 
+
+            for (int i = 0; i < nH; ++i) {
+                for (int j = 0; j < nW; ++j) { 
+                    double x = i * delta_x;  
+                    double y = j * delta_y; 
+                    nodes.push_back(Node(x, y)); 
+                }
+            }
+        }
+
+        void create_elements() {
+            for (int i = 0; i < nH - 1; ++i) {  
+                for (int j = 0; j < nW - 1; ++j) {  
+                    int node_1 = i * nW + j + 1;    
+                    int node_2 = node_1 + 1;             
+                    int node_3 = node_1 + nW + 1;       
+                    int node_4 = node_1 + nW;          
+
+                    Element element;                  
+                    element.set_ID(node_1, node_2, node_3, node_4); 
+                    elements.push_back(element);       
+                }
+            }
+        }
+
+        void display_grid_data() {
+            //cout << "Grid data: nN = " << nN << ", nE = " << nE << ", nH = " << nH << ", nW = " << nH <<  ", H = " << height << ", W = " << width << endl << endl; 
+
+            cout << "Nodes: " << endl << endl;
+            for (size_t i = 0; i < nodes.size(); i++) {
+                cout << "Node " << i + 1 << ": ";
+                nodes[i].display_node();
+            }
+            cout << "-----------------------------------" << endl;
+            cout << "Elements: " << endl << endl;
+            for (size_t i = 0; i < elements.size(); i++) {
+                cout << "Element " << i + 1 << ": ";
+                elements[i].display_ID();
+            }
+            cout << "-----------------------------------" << endl;
+        }
 };
 
-class GlobalData {
+class GlobalData { // dane do symulacji
     private:
-        double simulation_time, simulation_step_time, conductivity, alfa, tot, initial_temp, density, specific_heat, nN, nE;
+        double simulation_time, simulation_step_time, conductivity, alfa, tot, initial_temp, density, specific_heat, nN, nE, nH, nW, H, W;
     public:
         void read_file() {
             ifstream file("data.txt");
@@ -33,19 +116,29 @@ class GlobalData {
                 cout << "File not found." << endl;
             }
             
-            file >> simulation_time;
-            file >> simulation_step_time;
-            file >> conductivity;
-            file >> alfa;
-            file >> tot;
-            file >> initial_temp;
-            file >> density;
-            file >> specific_heat;
-            file >> nN;
-            file >> nE;
+            file >> simulation_time >> simulation_step_time >> conductivity >> alfa >> tot >> initial_temp >> density >> specific_heat >> nN >> nE >> nH >> nW >> H >> W;
             
             file.close();
+        }
 
+        double get_simulation_time() { return simulation_time; }
+        double get_simulation_step_time() { return simulation_step_time; }
+        double get_conductivity() { return conductivity; }
+        double get_alfa() { return alfa; }
+        double get_tot() { return tot; }
+        double get_initial_temp() { return initial_temp; }
+        double get_density() { return density; }
+        double get_specific_heat() { return specific_heat; }
+        double get_nN() { return nN; }
+        double get_nE() { return nE; }
+        double get_nH() { return nH; }
+        double get_nW() { return nW; }
+        double get_height() { return H; }
+        double get_width() { return W; }
+
+        void display_simulation_data() {
+            cout << "-----------------------------------" << endl;
+            cout << "Simulation data: " << endl << endl;
             cout << "Simulation time: " << simulation_time << endl;
             cout << "Simulation step time: " << simulation_step_time << endl;
             cout << "Conductivity: " << conductivity << endl;
@@ -56,13 +149,20 @@ class GlobalData {
             cout << "Specific heat: " << specific_heat << endl;
             cout << "Number of nodes: " << nN << endl;
             cout << "Number of elements: " << nE << endl;
+            cout << "Nodes height: " << nH << endl;
+            cout << "Nodes width: " << nW << endl;
+            cout << "Height: " << H << endl;
+            cout << "Width: " << W << endl;
+            cout << "-----------------------------------" << endl;
         }
 };
 
 int main() {
     GlobalData data;
     data.read_file();
-
+    Grid grid_1(data.get_nN(), data.get_nE(), data.get_nW(), data. get_nH(), data.get_height(), data.get_width());
+    data.display_simulation_data();
+    grid_1.display_grid_data();
 
     return 0;
 }
